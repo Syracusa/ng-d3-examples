da5f3bcba1cb1e43c23711147d0fd5cb07c15438
kj <msrndsy@gmail.com>
2023-01-31 07:27:17 +0900

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as d3 from 'd3'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-d3-examples';
+
+  testdata =
+    {
+      "2013-01-23": { "c": 1400, "h": 2233, "cpp": 4333 },
+      "2013-01-24": { "c": 1122, "h": 223, "cpp": 43 },
+      "2013-01-25": { "c": 400, "h": 223, "cpp": 432 },
+    }
+
+    
 }
